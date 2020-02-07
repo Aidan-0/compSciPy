f0290e48b2b27146315ac46592388d6094b21a74
Aidan-0 <Gamesitfo@gmail.com>
2020-02-07 09:20:03 -0600

    Added better language. Debugged and added features

--- 1.3/1.3 Final.py
+++ 1.3/1.3 Final.py
@@ -1,3 +1,4 @@
+# region lists
 SMB1 = ['Goomba', 'Mario', 'Luigi', 'Koopa', 'Peach', 'Bowser', 'Spiny']
 SMB2 = ['Mario', 'Luigi', 'Toad', 'Peach', 'Birdo']
 SMB3 = ['Mario', 'Luigi', 'Bowser', 'Thwomp', 'Goomba', 'Buzzy Beetle', 'Dry Bones', 'Lakitu', 'Hammer Bro']
@@ -5,42 +6,41 @@ SMW = ['Mario', 'Luigi', 'Yoshi', 'Peach', 'Thwomp', 'Goombrat', 'Cheep Cheep', 
 SM64 = ['Mario', 'Peach', 'Pokey', 'Bob-omb', 'Spiny', 'Chain Chomp', 'Goomba']
 NSMB = ['Mario', 'Luigi', 'Toad', 'Yoshi', 'Peach', 'Bowser Jr.', 'Bowser']
 SM3D = ['Mario', 'Luigi', 'Toad', 'Peach', 'Rosalina', 'Luma', 'Meowser', 'Big Boo', 'Goomba', 'Stingby']
+# endregion
 
-gameRecommendation = []
-gameRecommend = ''
-
-chrRecommendation = []
-chrTemp = ''
-chrRecommend = ''
-
-searchType = ''
-search = ''
-programQuery = ''
-
-
-def delTemp():
-    '''Used only for wiping temp variables, 
-    not the actual function :)'''
-    gameRecommend = ''
-    chrTemp = ''
-    chrRecommend = ''
-    searchType = ''
-    search = '' 
-    programQuery = ''
-    hits = 0
 
 def marioSearch():
+    '''Search through all of your favorite Super Mario Bros characters and what games they were from!
+       If I missed anyone/any game, make sure to suggest them using the suggestion feature!'''
+
+    # ! To anyone running, make sure that you're running this through Python 3, or it will not work !
+
+    # region variables
+    # Region and end are only for vscode code folding
     searchDone = False
-    programDone = False
+    # programDone = False
     hits = 0
+    gameRecommendation = []
+    gameRecommend = ''
+
+    chrRecommendation = []
+    chrTemp = ''
+    chrRecommend = ''
+
+    searchType = ''
+    search = ''
+    programQuery = ''
+    # endregion
 
     print('Welcome to Mario Bros search!')
-    while (programDone != True):
-        searchType = input('Please input either "character" or "game" to choose what you would like to search for: ')
-        if searchType == 'game':
+    while (searchDone != True):
+        searchType = (input('Please enter either "Character" or "Game" to choose what you would like to search for. To cancel, type "Cancel": ')).title()
+        if searchType == 'Game':
             while (searchDone != True):
 
-                search = input('Please choose what game you want (abbrevated): ')
+                search = (input('Please choose what game you want (abbrevated): ')).upper()
+                # region if-else for game
+                
                 if search == 'SMB1':
                     for string in SMB1:
                         print (string)
@@ -75,25 +75,25 @@ def marioSearch():
                     for string in SM3D:
                         print (string)
                     searchDone = True
-
+                # endregion
                 else:
-
-                    gameReccomend = input ('Sorry, that is not a valid game. Would you like to submit one? Y/N')
-                    if gameRecommend == 'Y':
-                        gameRecommendation += input ('Please input what game should be added (abbreveation): ')
-                        print ('Your game recommendation has been added!')
-                        delTemp()
-                        searchDone == True
-                    elif gameRecommend == 'N':
-                        print ('Ok!')
-                        delTemp()
-                    else:
-                        print ('That is not a valid option, please try again')
+                    while (searchDone != True):
+                        gameRecommend = input('Sorry, that is not a valid game. Would you like to submit this one? Y/N: ')
+                        if gameRecommend.upper() in ['Y', 'YES']:
+                            gameRecommendation.append(search.title())
+                            print (gameRecommendation)
+                            print ('Your game recommendation has been added!')
+                            searchDone = True
+                        elif gameRecommend.upper() in ['N', 'NO']:
+                            print ('Ok, no problem.')
+                            searchDone = True
+                        else:
+                            print ('That is not a valid option, please try again.')
                         
-        if searchType == 'character':
+        elif searchType == 'Character':
             while (searchDone != True):
-                search = input ('Please enter a character name: ')
-
+                # region for loops for character
+                search = (input('Please enter a character name: ')).title()
                 for string in SMB1:
                     if search == string:
                         print (string + ' - SMB1')
@@ -122,30 +122,39 @@ def marioSearch():
                     if search == string:
                         print (string + ' - SM3D')
                         hits += 1
-
+                # endregion 
+                
                 if hits == 0:
-                    chrRecommend = input ('Your search came out with no hits. Would you like to suggest a character? Y/N')
-                    if chrRecommend == 'Y':
-                        chrTemp += input ('Please input a character: ')
-                        chrTemp += ' -' + input ('What game are they from?: ')
-                        chrRecommendation += chrTemp
-                        print ('Thank you!')
-                        delTemp()
-                        searchDone = True
-                    elif chrRecommend == 'N':
-                        print ('Ok!')
-                        delTemp()
+                    while (searchDone != True):
+                        chrRecommend = input ('Your search came out with no hits. Would you like to suggest a character? Y/N: ')
+                        if chrRecommend.upper() in ['Y', 'YES']:
+                            chrTemp += search + ' - ' + (input('What game are they from?: ')).upper()
+                            chrRecommendation.append(chrTemp)
+                            print (chrRecommendation)
+                            print ('Thank you!')
+                            searchDone = True
+
+                        elif chrRecommend.upper() in ['N', 'NO']:
+                            print ('Ok!')
+                            searchDone = True
+
+                        else:
+                            print ('Sorry, that is not a valid option')
 
                 elif hits == 1:
                     print ('Your search came out with', 1, 'hit')
-                    delTemp()
                     searchDone = True
                     
                 else:
                     print ('Your search came out with', hits, 'hits')
-                    delTemp()
                     searchDone = True
-            
-        print ('Have a nice day :)')
-        delTemp()
-        programDone = True
+                    
+        elif searchType == 'Cancel':
+            print ('Ok, sorry about that!')
+            searchDone = True            
+        elif (searchDone != True):
+            print ('That input was not recognized, please try again')  
+
+    print ('Have a nice day :)')
+
+marioSearch()
