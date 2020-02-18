b363ac6ad170515d555c5dd41bc1e21c92516592
Aidan-0 <Gamesitfo@gmail.com>
2020-02-18 10:02:44 -0600

    Finshed 1.4.2

--- 1.4/1.4.2.py
+++ 1.4/1.4.2.py
@@ -14,22 +14,26 @@ filename = os.path.join(directory, 'cat1-a.gif')
 img = plt.imread(filename)
 '''Show the image data'''
 # Create figure with 1 subplot
-fig, ax = plt.subplots(1, 4)
+fig, ax = plt.subplots(1, 5)
 # Show the image data in a subplot
 
 ax[0].imshow(img, interpolation='none')
 
-ax[1].set_xlim(7, 17)
-ax[1].set_ylim(35, 45)
+ax[1].plot(43, 35, 'ro')
+ax[1].plot(68, 30, 'ro')
 ax[1].imshow(img, interpolation='none')
 
-ax[2].set_xlim(17, 27)
-ax[2].set_ylim(45, 55)
+ax[2].set_xlim(7, 17)
+ax[2].set_ylim(45, 35)
 ax[2].imshow(img, interpolation='none')
 
-ax[3].set_xlim(27, 37)
-ax[3].set_ylim(55, 65)
+ax[3].set_xlim(17, 27)
+ax[3].set_ylim(45, 35)
 ax[3].imshow(img, interpolation='none')
+
+ax[4].set_xlim(27, 37)
+ax[4].set_ylim(45, 35)
+ax[4].imshow(img, interpolation='none')
 
 # Show the figure on the screen
 fig.show()
