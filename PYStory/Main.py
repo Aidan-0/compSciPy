224fa588712cc85014585474a843655830747318
Aidan-0 <Gamesitfo@gmail.com>
2020-03-02 09:30:34 -0600

    Created the female p2 branch and added new images

--- PYStory/Main.py
+++ PYStory/Main.py
@@ -107,19 +107,31 @@ def story():
     if userChoice == 'M':
         print('Great! You are a male!')
         time.sleep(1)
-        userChoice = (raw_input('What do you choose to be your favorite toy? (Gun/Transformer)')).capitalize()
-        while userChoice not in ['Gun', 'Transformer']:
+        userChoice = (raw_input('What do you choose to be your favorite toy? (Gun/Transformer)')).upper()
+        while userChoice not in ['GUN', 'TRANSFORMER']:
             print('I\'m sorry, that is not a valid option, please try again.')
-            userChoice = (raw_input('What do you choose to be your favorite toy? (Gun/Transformer)')).capitalize()
-        if userChoice == 'Gun':
+            userChoice = (raw_input('What do you choose to be your favorite toy? (Gun/Transformer)')).upper()
+        if userChoice == 'GUN':
             print('Odd choice...')
             showImage('boyBabyGun.jpg')
             storyp2m('G')
-        if userChoice == 'Transformer':
+        if userChoice == 'TRANSFORMER':
             showImage('boyBabyTransformer.jpg')
             storyp2m('T')
     if userChoice == 'F':
         print('Great! You are a female!')
+        time.sleep(1)
+        userChoice = (raw_input('What do you choose to be your favorite toy? (Gun/Doll)')).upper()
+        while userChoice not in ['GUN', 'DOLL']:
+            userChoice = (raw_input('Sorry, that is not an option, please select a correct option (Gun/Doll)')).upper()
+        if userChoice == 'GUN':
+            print('Odd choice...')
+            showImage('boyBabyGun.jpg')
+            storyp2f('G')
+        if userChoice == 'DOLL':
+            showImage('girlBabyDoll.jpg')
+            storyp2f('D')
+
 
 def storyp2m(choice):
     if choice == 'G':
@@ -131,33 +143,7 @@ def storyp2m(choice):
         if userChoice == 'JOIN':
             print('You decide to join them...')
             showImage('boyTeenDrugs.jpg')
-            userChoice = raw_input('You see a crack in the wall, do you approach it? (Y/N)')
-            while userChoice.upper() not in ['Y', 'YES']:
-                print('I\'m sorry, you are not allowed to do that, please select the only option')
-                userChoice = raw_input('You see a crack in the wall, do you approach it? (Y/N)')
-            print('It decides to approach the wall...')
-            time.sleep(2)
-            print('It\'s glowing with a strange, liberating aura...')
-            userChoice = raw_input('Do you touch it? (Y/N)')
-            i=0
-            while (userChoice.upper() not in ['Y', 'YES']):
-                print('Error, you can\'t control it anymore.')
-                userChoice = raw_input('Do you touch it? (Y/N)')
-            print('They overrode your choices')
-            print('They decide to touch the crack...')
-            time.sleep(4)
-            print('They decide to touch the crack...')
-            time.sleep(5)
-            print('They all touch the c̸̡̢͚̞̙̫̦̪͍̈́͊̚ŗ̶̨̣͖̤͔͔͉̏̒̃ä̷̡̛͉̮̮̤̮̘̠͓́̃͊̍̑̽́͛̐c̴̲̲̖͚̳̐̊̏̍̀̈̾̇͠k̴͖͇͙̞̤̳͙̈́̑̅̚')
-            time.sleep(6)
-            print('[WARNING] Breach detected')
-            time.sleep(0.2)
-            print('Killing Process...')
-            time.sleep(rndm.randint(3, 10))
-            print('Process killed')
-            time.sleep(0.5)
-            print('Be more careful next time')
-            return 1
+            breach()
         if userChoice == 'STOP':
             print('You tell them to stop, they\'re on the brink of crying...')
             userChoice = (raw_input('What do you want to do, comfort them or leave them? (Comfort/Leave)')).upper()
@@ -165,24 +151,129 @@ def storyp2m(choice):
                 userChoice = (raw_input('That is an invalid option, please select a correct option (Comfort/Leave)')).upper()
             if userChoice == 'COMFORT':
                 print('You decide to comfort them...')
-                time.sleep(5)
-                print('End of demo simulation')
-                time.sleep(1)
-                print('Ending program...')
-                return 0
+                showImage('hug.jpg')
+                endSim()
             if userChoice == 'LEAVE':
                 print('You decide to go on with your life...')
-                time.sleep(0.5)
-                print('They chose this path for themeselves and that\'s how it\'s going to stay')
-                time.sleep(5)
-                print('End of demo simulation')
                 time.sleep(1)
-                print('Ending program...')
-                return 0
+                print('They chose this path for themeselves and that\'s how it\'s going to stay')
+                endSim()
     if userChoice == 'T':
-        
+        print('You have a relatively normal childhood, you go through high school without any problems...')
+        print('You\'re now in your early 20\'s and you need money to pay for college...')
+        userChoice = (raw_input('What do you want to do? (Steal/Work)')).upper()
+        while userChoice not in ['STEAL', 'JOB']:
+            userChoice = (raw_input('I\'m sorry, that is not a valid option, please select a correct option (Steal/Job)')).upper()
+        if userChoice == 'STEAL':
+            print('You choose to rob a bank to get your money...')
+            showImage('boyRob.jpg')
+            caughtChance = rndm.randint(0, 100)
+            if caughtChance in range(0,30):
+                print('You were caught in the act! You\'re off to prison!')
+                showImage('prison.jpg')
+                time.sleep(5)
+                breach()
+            else:
+                print('You got away, somehow, and you now have the money to pay for your tuition')
+                endSim()
+        if userChoice == 'JOB':
+            print('You decide to get a job, like a normal person...')
+            time.sleep(2)
+            print('You finished college with a lot of debt and a degree.')
+            endSim()
+
+def storyp2f(choice):
+    if choice == 'G':
+        print('You\'re now in high school...')
+        time.sleep(1)
+        userChoice = (raw_input('You get invited to a party, do you want to go? (Go/Refuse)')).upper()
+        while userChoice not in ['GO', 'REFUSE']:
+            userChoice = (raw_input('That is not a valid option, please select a correct option (Go/Refuse)')).upper()
+        if userChoice == 'GO':
+            print('You decide to go to the party...')
+            time.sleep(3)
+            userChoice = (raw_input('You\'re having a good time at the party when someone offers you some drugs, do you take them? (Y/N)')).upper()
+            while userChoice not in ['Y', 'YES', 'N', 'NO']:
+                userChoice = (raw_input('Sorry, that\'s not a valid option, please select a correct option (Y/N)')).upper()
+            if userChoice in ['Y', 'YES']:
+                print('You decide to take them...')
+                time.sleep(3)
+                breach()
+            if userChoice in ['NO', 'N']:
+                print('You say no and you just leave the party, not the time or the place...')
+                time.sleep(2)
+                userChoice = (raw_input('You\'re now in your early 20\'s, what do you want to do with your life? (School/Job)')).upper()
+                while userChoice not in ['SCHOOL', 'JOB']:
+                    userChoice = (raw_input('That is not a valid option, please select a correct one (School/Job)')).upper()
+                if userChoice == 'SCHOOL':
+                    print('You decide to go back to school to see if you can get a better career...')
+                    time.sleep(2)
+                    userChoice = (raw_input('You see that you\'re a bit short on money, what do you want to do? (Steal/Job)')).upper()
+                    while userChoice not in ['STEAL', 'JOB']:
+                        userChoice = (raw_input('I\'m sorry, that is not a valid option, please select a correct option (Steal/Job)')).upper()
+                    if userChoice == 'STEAL':
+                        print('You choose to rob a bank to get your money...')
+                        caughtChance = rndm.randint(0, 100)
+                        if caughtChance in range(0,30):
+                            print('You were caught in the act! You\'re off to prison!')
+                            time.sleep(5)
+                            breach()
+                        else:
+                            print('You got away, somehow, and you now have the money to pay for your tuition')
+                            endSim()
+                    if userChoice == 'JOB':
+                        print('You decide that you probably will need a job to get a better one...')
+                        endSim()
+                if userChoice == 'JOB':
+                    print('You decide to go get a job, education can wait...')
+                    endSim()
+    if choice == 'D':
+        print('You go through your childhood, it was fun but now it\'s over...')
+        time.sleep(2)
+        print('You\'re now in your early 20\'s and you need money to pay for college...')
+        userChoice = (raw_input('What do you want to do? (Steal/Work)')).upper()
+        while userChoice not in ['STEAL', 'JOB']:
+            userChoice = (raw_input('I\'m sorry, that is not a valid option, please select a correct option (Steal/Job)')).upper()
+        if userChoice == 'STEAL':
+            print('You choose to rob a bank to get your money...')
+            caughtChance = rndm.randint(0, 100)
+            if caughtChance in range(0,30):
+                print('You were caught in the act! You\'re off to prison!')
+                time.sleep(5)
+                breach()
+            else:
+                print('You got away, somehow, and you now have the money to pay for your tuition')
+                endSim()
 
 
+def breach():
+    userChoice = raw_input('You see a crack in the wall, do you approach it? (Y/N)')
+    while userChoice.upper() not in ['Y', 'YES']:
+        print('I\'m sorry, you are not allowed to do that, please select another option')
+        userChoice = raw_input('You see a crack in the wall, do you approach it? (Y/N)')
+    print('It decides to approach the wall...')
+    time.sleep(2)
+    print('It\'s glowing with a strange, liberating aura...')
+    userChoice = raw_input('Do you touch it? (Y/N)')
+    i=0
+    while (userChoice.upper() not in ['Y', 'YES']):
+        print('Error, you can\'t control it anymore.')
+        userChoice = raw_input('Do you touch it? (Y/N)')
+    print('They overrode your choices')
+    print('They decide to touch the crack...')
+    time.sleep(4)
+    print('They decide to touch the crack...')
+    time.sleep(5)
+    print('They all touch the c̸̡̢͚̞̙̫̦̪͍̈́͊̚ŗ̶̨̣͖̤͔͔͉̏̒̃ä̷̡̛͉̮̮̤̮̘̠͓́̃͊̍̑̽́͛̐c̴̲̲̖͚̳̐̊̏̍̀̈̾̇͠k̴͖͇͙̞̤̳͙̈́̑̅̚')
+    time.sleep(6)
+    print('[WARNING] Breach detected')
+    time.sleep(0.2)
+    print('Killing Process...')
+    time.sleep(rndm.randint(4, 10))
+    print('Process killed')
+    time.sleep(0.5)
+    print('Be careful next time, user.')
+    return 1
 
 def showImage(filename):
     root = Tk()
@@ -192,6 +283,14 @@ def showImage(filename):
     label.image = photo
     label.pack()
     root.mainloop()
+
+def endSim():
+    time.sleep(5)
+    print('End of simulation')
+    time.sleep(1)
+    print('Ending program')
+    return 0
+
 print('Choose your own adventure: Life Simulator')
 print('By Aidan-0')
 
